2b02f1326be859dbe77585b611bf7762cc0c10b8
corentin <corentinboleis@hotmail.fr>
2016-12-30 11:34:46 +0100

    dans TabbedPaneChapeter: supression de l'empty pane quand un autre est affiché. Affichage du dernier pane affiché.

--- src/ihm/viewers/TabbedPaneChapeter.java
+++ src/ihm/viewers/TabbedPaneChapeter.java
@@ -3,6 +3,7 @@
  */
 package ihm.viewers;
 
+import java.awt.Component;
 import java.awt.Dimension;
 
 import javax.swing.JPanel;
@@ -36,5 +37,16 @@ public class TabbedPaneChapeter extends JTabbedPane {
 			paneEmpty.setName("tab_empty");
 		}
 		return paneEmpty;
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.JTabbedPane#add(java.lang.String, java.awt.Component)
+	 */
+	@Override
+	public Component add(String arg0, Component arg1) {
+		this.remove(getPaneEmpty());
+		Component cmp =  super.add(arg0, arg1);
+		this.setSelectedIndex(this.getTabCount()-1);
+		return cmp;
 	}
 }
